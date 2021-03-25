1ccc3d67b0d9b40e950559f72dc3295bddf20f9e
Mediogre <caseyarrington13@gmail.com>
2021-03-24 20:00:31 -0500

    fetch and display quote

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// TODO
+
+// function for time (unsure of desired outcome as of now)
+
+// function for quote fetch
+// NOT USING QUOTES.rest here....
+const getQuote = function() {
+    // send fetch request
+    fetch("https://type.fit/api/quotes")
+      //then if ok parse response
+      .then(function(response) {
+          return response.json();
+      }).then(function(data) {
+          displayQuote(data);
+      } )
+}
+
+const displayQuote = function (dataArr) {
+    // find <p> by id
+    let quoteOfDayEl = $('#quoteOfTheDay');
+
+    // Choose a random index from data Arr
+    let randomIndexQuote = dataArr[Math.floor(Math.random() * dataArr.length)];
+
+    // change quoteOfDayEl text value to 
+    $(quoteOfDayEl).html(randomIndexQuote.text + "<span> -" + randomIndexQuote.author + "</span>");
+}
+
+getQuote();
